6abba2e47e1c7ddf7c936f009dbb2bbd03146fcd
KathleenEdie <kathleenedie@gmail.com>
2021-01-08 18:28:45 +0000

    sixth test completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -11,3 +11,7 @@ def add_or_remove_cash(cc_pet_shop, cash_1):
 
 def get_pets_sold(cc_pet_shop):
     return cc_pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(cc_pet_shop, pets_1):
+    cc_pet_shop["admin"]["pets_sold"] = cc_pet_shop["admin"]["pets_sold"] + pets_1
+    return get_pets_sold
